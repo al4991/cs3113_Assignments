0700bb615d55c06e38b711935670f26800a8c037
Andrew <al4991@nyu.edu>
2019-03-10 19:00:23 -0400

    Implementing Pong. So far, need to implement a victory mechanic

---
+++ hw2/NYUCodebase/main.cpp
@@ -0,0 +1,221 @@
+#ifdef _WINDOWS
+#include <GL/glew.h>
+#endif
+#include <SDL.h>
+#define GL_GLEXT_PROTOTYPES 1
+#include <SDL_opengl.h>
+#include <SDL_image.h>
+
+#ifdef _WINDOWS
+#define RESOURCE_FOLDER ""
+#else
+#define RESOURCE_FOLDER "NYUCodebase.app/Contents/Resources/"
+#endif
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include "ShaderProgram.h"
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include <math.h>
+
+SDL_Window* displayWindow;
+
+GLuint LoadTexture(const char *filePath) {
+	int w, h, comp;
+	unsigned char* image = stbi_load(filePath, &w, &h, &comp, STBI_rgb_alpha);
+
+	if (image == NULL) {
+		std::cout << "Unable to load image. Make sure the path is correct\n";
+
+	}
+
+	GLuint retTexture;
+	glGenTextures(1, &retTexture);
+	glBindTexture(GL_TEXTURE_2D, retTexture);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	stbi_image_free(image);
+	return retTexture;
+}
+
+class Object {
+
+public: 
+	float x_pos = 0.0f; 
+	float y_pos = 0.0f;
+	float half_width; 
+	float half_height; 
+	float x_dir; 
+	float y_dir; 
+
+	Object(float width, float height, float x=0.0f, float y=0.0f) {
+		half_height = height / 2; 
+		half_width = width / 2; 
+		x_pos = x;
+		y_pos = y;
+		x_dir = 0.0f;
+		y_dir = 0.0f; 
+	}
+
+	void draw(ShaderProgram& p) {
+		glm::mat4 modelMatrix = glm::mat4(1.0f); 
+		modelMatrix = glm::translate(modelMatrix, glm::vec3(x_pos, y_pos, 0.0f)); 
+		p.SetModelMatrix(modelMatrix); 
+
+		float vertices[] = {
+			-half_width, -half_height, 
+			half_width, -half_height, 
+			half_width, half_height, 
+			-half_width, -half_height, 
+			half_width, half_height, 
+			-half_width, half_height, 
+		};
+
+		glVertexAttribPointer(p.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+		glEnableVertexAttribArray(p.positionAttribute);
+
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDisableVertexAttribArray(p.positionAttribute);
+	}
+
+	
+	void setRandomDir() {
+		x_dir = -1.0f; 
+		y_dir = 1.0f; 
+	}
+
+	void move(float elapsed) {
+		x_pos += (elapsed * 0.5f * x_dir); 
+		y_pos += (elapsed * 0.5f * y_dir); 
+	}
+};
+bool collisionCheck(Object& obj1, Object& obj2) {
+	float x_distance = fabs(obj1.x_pos - obj2.x_pos) - ((obj1.half_width + obj2.half_width));
+	float y_distance = fabs(obj1.y_pos - obj2.y_pos) - ((obj1.half_height + obj2.half_height));
+	if (x_distance <= 0 && y_distance <= 0) {
+		return true;
+	}
+	return false;
+}
+
+void draw_midline(ShaderProgram& p) {
+	Object block = Object(0.07f, 0.07f, 0.0f, 0.95f);
+	for (int i = 0; i < 15; i++) {
+		block.draw(p); 
+		block.y_pos -= 0.135f;
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	SDL_Init(SDL_INIT_VIDEO);
+	displayWindow = SDL_CreateWindow("yote", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 960, 540, SDL_WINDOW_OPENGL);
+	SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
+	SDL_GL_MakeCurrent(displayWindow, context);
+
+#ifdef _WINDOWS
+	glewInit();
+#endif
+	glViewport(0, 0, 960, 540);
+	ShaderProgram program;
+	program.Load(RESOURCE_FOLDER"vertex.glsl", RESOURCE_FOLDER"fragment.glsl");
+
+	float lastFrameTicks = 0.0f;
+
+	glm::mat4 projectionMatrix = glm::mat4(1.0f);
+	glm::mat4 modelMatrix = glm::mat4(1.0f);
+	glm::mat4 viewMatrix = glm::mat4(1.0f);
+
+	projectionMatrix = glm::ortho(-1.777f, 1.777f, -1.0f, 1.0f, -1.0f, 1.0f);
+	glUseProgram(program.programID);
+
+	// Creating objects 
+	Object paddle1 = Object(0.07f, 0.25f, -1.465f);
+	Object paddle2 = Object(0.07f, 0.25f, 1.465f);
+	paddle2.y_dir = 1.0f; 
+
+	Object border1 = Object(3.0f, 0.05f, 0.0f, 0.975f);
+	Object border2 = Object(3.0f, 0.05f, 0.0f, -0.975f);
+
+	Object ball = Object(0.075f, 0.075f);
+	ball.setRandomDir(); 
+
+	SDL_Event event;
+	bool done = false;
+	while (!done) {
+
+		float ticks = (float)SDL_GetTicks() / 1000.0f;
+		float elapsed = ticks - lastFrameTicks;
+		lastFrameTicks = ticks;
+		const Uint8 *keys = SDL_GetKeyboardState(NULL);
+
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
+				done = true;
+			}
+		}
+		if (keys[SDL_SCANCODE_UP] || keys[SDL_SCANCODE_W]) {
+			if (!collisionCheck(paddle1, border1)) {
+				paddle1.y_pos += (elapsed * .7);
+
+			}
+		}
+		else if (keys[SDL_SCANCODE_DOWN] || keys[SDL_SCANCODE_S]) {
+			
+			if (!collisionCheck(paddle1, border2)) {
+				paddle1.y_pos -= (elapsed * .7);
+
+			}
+		}
+        glClear(GL_COLOR_BUFFER_BIT);
+
+		program.SetProjectionMatrix(projectionMatrix);
+		program.SetViewMatrix(viewMatrix);
+		
+		glUseProgram(program.programID); 
+//=======================================================================================
+		// Ball 
+		if (collisionCheck(ball, border1) || collisionCheck(ball, border2)) {
+			ball.y_dir *= -1.0f;
+		}
+		if (collisionCheck(ball, paddle1) || collisionCheck(ball, paddle2)) {
+			ball.x_dir *= -1.0f;
+		}
+		ball.move(elapsed);
+		ball.draw(program);
+
+		// Borders
+		border1.draw(program); 
+		border2.draw(program); 
+
+		// Paddles 
+		paddle1.draw(program); 
+		paddle2.y_pos += ((elapsed * 3.0f) * paddle2.y_dir); 
+		if (collisionCheck(paddle2, border1) || collisionCheck(paddle2, border2)) {
+			paddle2.y_dir *= -1.0f;
+		}
+		paddle2.draw(program); 
+
+
+		// Midline
+		draw_midline(program); 
+
+		
+	
+		
+
+
+		// End of the line pal. Nothing after this point. 
+		SDL_GL_SwapWindow(displayWindow);
+    }
+
+
+	
+    
+    SDL_Quit();
+    return 0;
+}
